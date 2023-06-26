911d77e680ab410aac4f60250ddeeddf4b6deeec
C1rdec <cedric_lampron@hotmail.com>
2023-06-26 13:17:46 -0400

    First commit

---
+++ Lurker.BattleNet/Models/BattleNetGame.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Lurker.Common.Models;
+
+namespace Lurker.BattleNet.Models
+{
+    public class BattleNetGame : GameBase
+    {
+        private readonly string _installationFolder;
+
+        public BattleNetGame(string installationFolder)
+        {
+            _installationFolder = installationFolder;
+            Name = Path.GetFileName(_installationFolder);
+        }
+
+        public override LauncherType Launcher => LauncherType.BattleNet;
+
+        public override string Id => _installationFolder;
+
+        public override void Initialize()
+        {
+            SetExeFile(_installationFolder, "Launcher");
+        }
+
+        public override Task Open()
+            => CliWrap.Cli.Wrap(ExeFilePath).ExecuteAsync();
+    }
+}
---
+++ Lurker.BattleNet/Services/BattleNetService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using Lurker.BattleNet.Models;
+using Lurker.Common.Services;
+using static System.Environment;
+
+namespace Lurker.BattleNet.Services
+{
+    public class BattleNetService : ServiceBase<BattleNetGame>
+    {
+        protected override string ProcessName => "BattleNet";
+
+        protected override string OpenLink => string.Empty;
+
+        private static string ConfigFilePath => Path.Combine(GetFolderPath(SpecialFolder.ApplicationData), "Battle.net", "Battle.net.config");
+
+        public override List<BattleNetGame> FindGames()
+        {
+            var games = new List<BattleNetGame>();
+            if (!File.Exists(ConfigFilePath))
+            {
+                return games;
+            }
+
+            var document = JsonDocument.Parse(File.ReadAllText(ConfigFilePath));
+            var defaultInstallPath = document.RootElement.GetProperty("Client").GetProperty("Install").GetProperty("DefaultInstallPath").GetString();
+
+            if (string.IsNullOrEmpty(defaultInstallPath))
+            {
+                return games;
+            }
+
+            foreach (var directory in Directory.GetDirectories(defaultInstallPath))
+            {
+                var game = new BattleNetGame(directory);
+                game.Initialize();
+                if (string.IsNullOrEmpty(game.ExeFilePath))
+                {
+                    continue;
+                }
+
+                games.Add(game);
+            }
+
+            return games;
+        }
+    }
+}
